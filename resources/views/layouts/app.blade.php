2a0078172e595b1e07d7c89af7a6aaee8777f4a5
LordBluue3 <mikaelcandido71@gmail.com>
2023-09-08 18:03:11 -0300

    feat: create footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,60 @@
 
     <footer>
 
+        <div class="container_itens_footer">
+            <div class="container_footer_top">
+                <div class="container_informations">
+                    <div class="container_logo">
+                        <img src=" {{asset('assets/images/logos/logo_mob.svg')}} " alt="Logo Real Hosting">
+                    </div>
+                    <div class="container_about">
+                        <p>A empresa tem a intenção de prover o serviço 
+                            de hospedagem de servidores, inicialmente de 
+                            Minecraft e, posteriormente de vários outros 
+                            tipos de jogos, como FiveM, MTA, Minecraft 
+                            Bedrock, etc...</p>
+                    </div>
+                    <div class="container_quality">
+                        <p>Qualidades e proteções contra qualquer tipo de
+                            ataques em nossos servidores.</p>
+                    </div>
+                </div>
+        
+                <div class="container_services_suport">
+                    <div class="container_services">
+                        <h2>Serviços</h2>
+                        <ul>
+                            <li>Multi Theft Auto - MTA</li>
+                            <li>Minecraft</li>
+                            <li>FiveM</li>
+                            <li>Minecraft Bedrock</li>
+                            <li>Outros</li>
+                        </ul>
+        
+                    </div>
+                    <div class="container_suport">
+                        <h2>Suporte</h2>
+                        <ul>
+                            <li>Área do cliente</li>
+                            <li>Email</li>
+                            <li>Discord</li>
+                            <li>Termos e Serviços</li>
+                        </ul>
+        
+                    </div>
+                </div>
+            </div>
+            <div class="container_copy_terms">
+                <div class="container_copy">
+                    <p>RealHosting Hospedagem Ltda. Copyright © 2021 - 2022</p>
+                </div>
+                <div class="container_terms">
+                    <p>Termos & Políticas</p>
+                </div>
+            </div>
+
+        </div>
+
     </footer>
 </body>
 

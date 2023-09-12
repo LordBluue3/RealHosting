cd1e124a97bb68abde4ceac59d19876326f38692
Mikael <mikaelcandido71@gmail.com>
2023-09-12 12:44:52 -0300

    feat: create event listener for carousel product

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,4 +1,5 @@
 const carousel = document.querySelector('.carousel');
+const carouselProduct = document.querySelector('.carousel_product');
 let isDragging = false;
 let startX, startScrollLeft;
 
@@ -18,4 +19,24 @@ carousel.addEventListener("mousemove", (e)=>{
 document.addEventListener("mouseup", ()=>{
     isDragging = false;
     carousel.classList.remove("dragging");
+});
+
+//Carousel Product
+
+carouselProduct.addEventListener("mousedown", (e)=>{
+    isDragging = true;
+    carouselProduct.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carouselProduct.scrollLeft;
+});
+
+carouselProduct.addEventListener("mousemove", (e)=>{
+    if(!isDragging) return;
+    carouselProduct.scrollLeft =  startScrollLeft - (e.pageX - startX);
+    
+});
+
+document.addEventListener("mouseup", ()=>{
+    isDragging = false;
+    carouselProduct.classList.remove("dragging");
 });

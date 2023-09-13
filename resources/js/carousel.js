c9280f2d06857cfa051ce738df6d59bf8d138d47
Mikael <mikaelcandido71@gmail.com>
2023-09-13 11:18:45 -0300

    refactor: remove duplicate listener and create generic function

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,63 +1,28 @@
-const carousel = document.querySelector('.carousel');
-const carouselProduct = document.querySelector('.carousel_product');
-const carouselTestimony = document.querySelector('.carousel_testimony');
-let isDragging = false;
-let startX, startScrollLeft;
+const carousel = document.querySelector(".carousel");
+const carouselProduct = document.querySelector(".carousel_product");
+const carouselTestimony = document.querySelector(".carousel_testimony");
 
-carousel.addEventListener("mousedown", (e)=>{
-    isDragging = true;
-    carousel.classList.add("dragging");
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-});
+function setupCarousel(carousel) {
+    let isDragging = false;
+    let startX, startScrollLeft;
+    carousel.addEventListener("mousedown", (e) => {
+        isDragging = true;
+        carousel.classList.add("dragging");
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
+    });
 
-carousel.addEventListener("mousemove", (e)=>{
-    if(!isDragging) return;
-    carousel.scrollLeft =  startScrollLeft - (e.pageX - startX);
-    
-});
+    carousel.addEventListener("mousemove", (e) => {
+        if (!isDragging) return;
+        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    });
 
-document.addEventListener("mouseup", ()=>{
-    isDragging = false;
-    carousel.classList.remove("dragging");
-});
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        carousel.classList.remove("dragging");
+    });
+}
 
-//Carousel Product
-
-carouselProduct.addEventListener("mousedown", (e)=>{
-    isDragging = true;
-    carouselProduct.classList.add("dragging");
-    startX = e.pageX;
-    startScrollLeft = carouselProduct.scrollLeft;
-});
-
-carouselProduct.addEventListener("mousemove", (e)=>{
-    if(!isDragging) return;
-    carouselProduct.scrollLeft =  startScrollLeft - (e.pageX - startX);
-    
-});
-
-document.addEventListener("mouseup", ()=>{
-    isDragging = false;
-    carouselProduct.classList.remove("dragging");
-});
-
-//Carousel Testimony
-
-carouselTestimony.addEventListener("mousedown", (e)=>{
-    isDragging = true;
-    carouselTestimony.classList.add("dragging");
-    startX = e.pageX;
-    startScrollLeft = carouselTestimony.scrollLeft;
-});
-
-carouselTestimony.addEventListener("mousemove", (e)=>{
-    if(!isDragging) return;
-    carouselTestimony.scrollLeft =  startScrollLeft - (e.pageX - startX);
-    
-});
-
-document.addEventListener("mouseup", ()=>{
-    isDragging = false;
-    carouselTestimony.classList.remove("dragging");
-});
+setupCarousel(carousel)
+setupCarousel(carouselProduct)
+setupCarousel(carouselTestimony)

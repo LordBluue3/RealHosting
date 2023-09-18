d880c28ac6fda4127eea8043cf96ad4f1068bf34
Mikael <mikaelcandido71@gmail.com>
2023-09-18 15:41:00 -0300

    feat: create function index in the products controller for return the view

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        $data = [
+            'title'=> 'Escolha um dos nossos serviços e aproveite nossas qualidades ',
+            'text'=> 'Promova hoje seu servidor com uma hospedagem com uma alta qualidade e proteção contra qualquer tipo de ataque.'
+        ];
+
+        $product = [ 
+            'image'=>'/assets/images/products/image1.png',
+            'title'=>'Hospedagem de MTA',
+            'price'=>'9,99',
+            'description'=>'Servidores MTA com um sistema 
+            inovador de compactador e download
+            e hospedagem totalmente Brasileira.'
+        ];
+
+        return view('pages.products', compact('data', 'product'));
+    }
+}

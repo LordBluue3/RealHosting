f262db02b697eec959eef52f261fc5d0d3f42624
Mikael <mikaelcandido71@gmail.com>
2023-09-18 15:36:59 -0300

    build: edit the scss folder in the vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/scss/*.scss', 'resources/js/app.js', 'resources/views/**/*.blade.php'],
+      input: ['resources/scss/**/*.scss', 'resources/js/app.js', 'resources/views/**/*.blade.php'],
       refresh: true,
     }),
   ],

0039c46b42b88717f786350e66bdc4592bd374df
Mikael <mikaelcandido71@gmail.com>
2023-09-20 16:14:35 -0300

    feat: create login page

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/app')
+@push('head')
+@vite(['resources/scss/pages/login.scss'])
+@endpush
+
+@section('content')
+<section class="section_login">
+    <div class="container_login">
+        <div class="container_title">
+            <h1>Entre na sua conta</h1>
+        </div>
+
+        <form action="">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Senha">
+            <div class="container_button">
+                <p>Não tem uma conta? <a href="{{route('register')}}">Cadastre-se</a> </p>
+                <button type="submit">Logar</button>
+            </div>
+        </form>
+
+    </div>
+</section>
+@endsection

efa6e7d7edbb7a488e955a1616fc26663823630a
Mikael <mikaelcandido71@gmail.com>
2023-09-21 11:21:36 -0300

    feat: create method index what return the view product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'title'=> 'Confira todos nossos planos para
+            Hospedagem de Minecraft',
+            'text'=> 'Promova hoje seu servidor com uma hospedagem com uma
+            alta qualidade e proteção contra qualquer tipo de ataque.'
+        ];
+        $specificProduct = [
+            'image'=>'/assets/images/products/image1.png',
+            'title'=>'Hospedagem de MTA',
+            'price'=>'9,99',
+            'description'=>'Servidores MTA com um sistema 
+            inovador de compactador e download
+            e hospedagem totalmente Brasileira.'
+
+        ];
+        return view('pages/product', compact('data','specificProduct'));
+    }
+}

1e0fc105ff036111a8ef2a605e5c72312ef208d6
Mikael <mikaelcandido71@gmail.com>
2023-09-21 11:19:07 -0300

    feat: structure the section cards

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts/app')
 
 @push('head')
-
+    @vite(['resources/scss/pages/products.scss'])
 @endpush
 
 @section('content')
-
+    @include('components.titleServices', $data)
+    <section class="section_cards">
+        <div class="container_cards">
+            @for ($i = 0; $i <= 6; $i++)
+                @include('components.cardProducts', $specificProduct)
+            @endfor
+        </div>
+    </section>
 @endsection

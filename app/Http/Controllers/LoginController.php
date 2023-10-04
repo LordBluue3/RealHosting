ebfccaa6bbc7a93f7b997c4a294979ca5a0b4b53
Mikael <mikaelcandido71@gmail.com>
2023-10-04 16:16:49 -0300

    feat: create store method
    - this method verify if the user or admin are autheticated
    
    - return the dashboard of the admin or user
    
    - if the user or admin don't are authenticated, redirect for login page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function show()
     {
         return view('pages/login');
+    }
+    public function store(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+        
+        if(Auth::guard('web')->attempt(['email' => $email, 'password' => $password]))
+        {
+            return redirect('/dashboard/user/');
+        }else if(Auth::guard('admin')->attempt(['email' => $email, 'password'=> $password])){
+            return redirect('/dashboard/admin/');
+        }
+        else{
+            return redirect()->back();
+        }
+      
     }
 }

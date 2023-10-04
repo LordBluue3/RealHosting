28e31b8c6e179eab4dac53e49469872b0a14cada
Mikael <mikaelcandido71@gmail.com>
2023-10-04 16:22:17 -0300

    feat: create the method store
    - this method get the value of the inputs
    
    - make the password hash
    
    - create the new user, inserting the name, email and password
    
    -return view products

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function show()
     {
         return view('pages/register');
+    }
+    public function store(Request $request){
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $passwordHash = Hash::make($password);
+
+        $user = User::create([
+            'name'=> $name,
+            'email'=>$email,
+            'password'=>$passwordHash
+        ]);
+        $user->save();
+
+
+        $data = [
+            'title'=> 'Escolha um dos nossos serviços e aproveite nossas qualidades ',
+            'text'=> 'Promova hoje seu servidor com uma hospedagem com uma alta qualidade e proteção contra qualquer tipo de ataque.'
+        ];
+
+        $product = [ 
+            'image'=>'/assets/images/products/image1.png',
+            'title'=>'Hospedagem de MTA',
+            'price'=>'9,99',
+            'description'=>'Servidores MTA com um sistema 
+            inovador de compactador e download
+            e hospedagem totalmente Brasileira.'
+        ];
+        return view('pages/products', compact('data', 'product'));
     }
 }

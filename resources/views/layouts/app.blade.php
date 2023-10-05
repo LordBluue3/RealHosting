1a564d2109f4e300d2cc5aab854014687b16f0db
Mikael <mikaelcandido71@gmail.com>
2023-10-05 16:42:20 -0300

    feat: add title and meta charset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="pt-br">
 
 <head>
+    <title></title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width initial-scale=1.0">
     @vite(['resources/scss/layout/app.scss'])
-
     @stack('head')
 </head>
 

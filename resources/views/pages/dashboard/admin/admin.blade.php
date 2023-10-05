c5536fe10cb60d933032cbb55d1a9131ca20248c
Mikael <mikaelcandido71@gmail.com>
2023-10-05 16:45:30 -0300

    feat: add the home dashboard logo

---
+++ resources/views/pages/dashboard/admin/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/dashboard_admin')
+
+@push('head')
+
+@endpush
+
+
+@section('content')
+<img style="width: 150px" src="{{ asset('assets/images/logos/logo_isotipo.png') }}" alt="">
+<h1 >Dashboard admin</h1>
+@endsection
